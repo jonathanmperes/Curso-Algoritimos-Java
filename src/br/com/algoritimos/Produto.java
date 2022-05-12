0181982d28e202a17bd01d04a6a15d070285bef4
jonathanmperes <emailmarceloperes@gmail.com>
2022-05-12 17:04:03 -0300

    Busca do menor valor no array

--- src/br/com/algoritimos/Produto.java
+++ src/br/com/algoritimos/Produto.java
@@ -1,2 +1,21 @@
-package br.com.algoritimos;public class Produto {
+package br.com.algoritimos;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, int preco) {
+        this.nome = nome;
+        this.preco = preco;
+
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
